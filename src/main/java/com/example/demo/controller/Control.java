fef8d1e8a5f1d4a6559527c1710445f91252e062
Zsebssx <sebastianisle18@gmail.com>
2025-06-12 20:05:14 -0500

    modifique un mensaje

--- src/main/java/com/example/demo/controller/Control.java
+++ src/main/java/com/example/demo/controller/Control.java
@@ -13,7 +13,7 @@ public class Control {
     @GetMapping("/mensaje")
     public Map<String, String> obtenerMensaje() {
         Map<String, String> respuesta = new HashMap<>();
-        respuesta.put("mensaje", "Hola, este es un PROFE DE MIERA");
+        respuesta.put("mensaje", "Hola, este es un PROFE DE MIERda hijo de perra con su delegado chupapenes que se deja bajar el pantalon por el profe");
         return respuesta;
     }
 
